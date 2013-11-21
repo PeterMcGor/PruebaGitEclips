bed0797a709c4ef17f273588750e5b76eaf544f3
peter <pedro.macias.gordaliza@gmail.com>
2013-11-21 02:57:51 +0100

    Nothing changes

--- hola/src/HelloWorld.java
+++ hola/src/HelloWorld.java
@@ -1,10 +1,12 @@
 
+
 public class HelloWorld {
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) {	
+		
 		//Changing
 		System.out.println("Hola Mundo");
 		//rama2
